705b4dd86729ae2cdb99902f74aa4afd79abef2a
Mashrafi <rangdhanu322@gmail.com>
2025-11-20 10:38:30 +0600

    Add Flask routes and HTML template for greeting functionality

--- app.py
+++ app.py
@@ -1,6 +1,15 @@
 from flask import Flask
+from flask import render_template
+from datetime import datetime
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "Hello !"
+@app.route("/hello/")
+@app.route("/hello/<name>")
+def hello_there(name = None):
+    return render_template(
+        "hello_there.html",
+        name=name,
+        date=datetime.now()
+    )
+
+
